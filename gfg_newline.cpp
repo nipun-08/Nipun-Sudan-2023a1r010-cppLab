744ed670bcc9421732bbcc1a07f1654ec7b466d6
nipun-08 <146643057+nipun-08@users.noreply.github.com>
2025-03-03 14:25:25 +0530

    Add files via upload

---
+++ gfg_newline.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+class Hello
+{
+    public: void H()
+    {
+        cout<<"Geeks\nfor\nGeeks";
+    }
+};
+int main()
+{
+    Hello obj;
+    obj.H();
+}
